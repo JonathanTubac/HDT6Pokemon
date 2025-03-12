afec59e7e3e5897eb71d1aac6d083a30abc008e6
Jonathan <144759872+JonathanTubac@users.noreply.github.com>
2025-03-12 02:09:13 -0600

    javadocs

--- pokemon-hdt6/src/main/java/uvg/edu/Pokemon.java
+++ pokemon-hdt6/src/main/java/uvg/edu/Pokemon.java
@@ -1,7 +1,13 @@
+/**
+ * Jonathan Javier Tubac Arreaza 24484
+ */
 package uvg.edu;
 
 import java.util.List;
 
+/**
+ * The Pokemon class represents a Pokémon with various attributes such as name, types, classification, height, weight, abilities, generation, and legendary status.
+ */
 public class Pokemon {
     private String name;
     private int pokedex_num;
@@ -11,12 +17,26 @@ public class Pokemon {
     private double height_meters;
     private double weight_kg;
     private List<String> abilities;
-    private String generation;
+    private int generation;
     private String is_legendary;
 
-    // Constructor con los parámetros correctos
-    public Pokemon(String name, int pokedex_num, String type1, String type2, String classification,
-            double height_meters, double weight_kg, List<String> abilities, String generation, String is_legendary) {
+    /**
+     * Constructs a new Pokemon with the specified attributes.
+     *
+     * @param name The name of the Pokémon.
+     * @param pokedex_num The Pokédex number of the Pokémon.
+     * @param type1 The primary type of the Pokémon.
+     * @param type2 The secondary type of the Pokémon (can be null).
+     * @param classification The classification of the Pokémon.
+     * @param height_meters The height of the Pokémon in meters.
+     * @param weight_kg The weight of the Pokémon in kilograms.
+     * @param abilities The list of abilities of the Pokémon.
+     * @param generation The generation of the Pokémon.
+     * @param is_legendary The legendary status of the Pokémon.
+     */
+    public Pokemon(String name, int pokedex_num, String type1, String type2, 
+                   String classification, double height_meters, double weight_kg, 
+                   List<String> abilities, int generation, String is_legendary) {
         this.name = name;
         this.pokedex_num = pokedex_num;
         this.type1 = type1;
@@ -29,47 +49,101 @@ public class Pokemon {
         this.is_legendary = is_legendary;
     }
 
-    // Getters y setters (si es necesario)
+    /**
+     * Gets the name of the Pokémon.
+     *
+     * @return The name of the Pokémon.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the Pokédex number of the Pokémon.
+     *
+     * @return The Pokédex number of the Pokémon.
+     */
     public int getPokedexNum() {
         return pokedex_num;
     }
 
+    /**
+     * Gets the primary type of the Pokémon.
+     *
+     * @return The primary type of the Pokémon.
+     */
     public String getType1() {
         return type1;
     }
 
+    /**
+     * Gets the secondary type of the Pokémon.
+     *
+     * @return The secondary type of the Pokémon.
+     */
     public String getType2() {
         return type2;
     }
 
+    /**
+     * Gets the classification of the Pokémon.
+     *
+     * @return The classification of the Pokémon.
+     */
     public String getClassification() {
         return classification;
     }
 
+    /**
+     * Gets the height of the Pokémon in meters.
+     *
+     * @return The height of the Pokémon in meters.
+     */
     public double getHeightMeters() {
         return height_meters;
     }
 
+    /**
+     * Gets the weight of the Pokémon in kilograms.
+     *
+     * @return The weight of the Pokémon in kilograms.
+     */
     public double getWeightKg() {
         return weight_kg;
     }
 
+    /**
+     * Gets the list of abilities of the Pokémon.
+     *
+     * @return The list of abilities of the Pokémon.
+     */
     public List<String> getAbilities() {
         return abilities;
     }
 
-    public String getGeneration() {
+    /**
+     * Gets the generation of the Pokémon.
+     *
+     * @return The generation of the Pokémon.
+     */
+    public int getGeneration() {
         return generation;
     }
 
+    /**
+     * Gets the legendary status of the Pokémon.
+     *
+     * @return The legendary status of the Pokémon.
+     */
     public String getIsLegendary() {
         return is_legendary;
     }
 
+    /**
+     * Returns a string representation of the Pokémon.
+     *
+     * @return A string representation of the Pokémon.
+     */
     @Override
     public String toString() {
         return "Nombre: " + name + ", Pokedex Number: " + pokedex_num +
